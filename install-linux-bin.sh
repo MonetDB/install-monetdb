76a199844f205515f38fc74c5c74a61da5595e52
Joeri van Ruth <joeri.van.ruth@monetdbsolutions.com>
2024-11-20 16:01:46 +0100

    Add rpm support

--- install-linux-bin.sh
+++ install-linux-bin.sh
@@ -2,60 +2,89 @@
 
 set -e -x
 
-source "/etc/os-release"
-suite="$VERSION_CODENAME"
-test -n "$suite"
+REQUESTED_MONETDB_VERSION="${1?}"
 
-VERSION="${1?}"
 
-{
-    echo deb https://dev.monetdb.org/downloads/deb/ "$suite" monetdb
-    echo deb-src https://dev.monetdb.org/downloads/deb/ "$suite" monetdb
-} | sudo dd of=/etc/apt/sources.list.d/monetdb.list
+install_debs() {
+    source "/etc/os-release"
+    suite="$VERSION_CODENAME"
+    test -n "$suite"
 
-sudo wget --output-document=/etc/apt/trusted.gpg.d/monetdb.gpg https://dev.monetdb.org/downloads/MonetDB-GPG-KEY.gpg
+    {
+        echo deb https://dev.monetdb.org/downloads/deb/ "$suite" monetdb
+        echo deb-src https://dev.monetdb.org/downloads/deb/ "$suite" monetdb
+    } | sudo dd of=/etc/apt/sources.list.d/monetdb.list
 
-sudo apt-get -qy update
-sudo apt-get -qy install pkg-config libz-dev
+    sudo wget --output-document=/etc/apt/trusted.gpg.d/monetdb.gpg https://dev.monetdb.org/downloads/MonetDB-GPG-KEY.gpg
 
-initial_packages=(
-    monetdb-server
-    monetdb-sql
-    monetdb-client
-    libmonetdbe-dev
-)
+    sudo apt-get -qy update
+    sudo apt-get -qy install pkg-config libz-dev
 
-# If we ask apt-get to install a specific version of a package we also need
-# explicitly install all its dependencies at the right version.
-# Because the name of the depencies varies (libmonetdb-stream26/27/28)
-# we cannot just put a static list here.
-#
-# There are commands that recursively resolve the dependencies and there
-# are tools that support versions but the following loop is the best
-# I could come up with that does both.
-packages=("${initial_packages[@]/%/=$VERSION}")
-count=0
-while test $count -ne ${#packages[@]}; do
-    count=${#packages[@]}
-    apt depends "${packages[@]}" \
-        | sed -n -e '/monet/s/Depends: \([^ ]*\) ([^ ]* \(.*\)).*/\1=\2/p' \
-        | tee scratch
-    for p in "${packages[@]}"; do
-        echo "$p" >>scratch
+    initial_packages=(
+        monetdb-server
+        monetdb-sql
+        monetdb-client
+        libmonetdbe-dev
+    )
+
+    # If we ask apt-get to install a specific version of a package we also need
+    # explicitly install all its dependencies at the right version.
+    # Because the name of the depencies varies (libmonetdb-stream26/27/28)
+    # we cannot just put a static list here.
+    #
+    # There are commands that recursively resolve the dependencies and there
+    # are tools that support versions but the following loop is the best
+    # I could come up with that does both.
+    packages=("${initial_packages[@]/%/=$REQUESTED_MONETDB_VERSION}")
+    count=0
+    while test $count -ne ${#packages[@]}; do
+        count=${#packages[@]}
+        apt depends "${packages[@]}" \
+            | sed -n -e '/monet/s/Depends: \([^ ]*\) ([^ ]* \(.*\)).*/\1=\2/p' \
+            | tee /tmp/scratch
+        for p in "${packages[@]}"; do
+            echo "$p" >>/tmp/scratch
+        done
+        packages=($(sed -e 's/ //g' /tmp/scratch | sort -u))
     done
-    packages=($(sed -e 's/ //g' scratch | sort -u))
-done
 
-# Now install the full list of packages we determined
-sudo apt-get -qy install "${packages[@]}"
+    # Now install the full list of packages we determined
+    sudo apt-get -qy install "${packages[@]}"
+}
 
 
-# Borrow exact installation directory from libz
+install_rpms() {
+    yum -y install https://dev.monetdb.org/downloads/epel/MonetDB-release-epel.noarch.rpm
+    rpm --import https://dev.monetdb.org/downloads/MonetDB-GPG-KEY
+    pkgs=(
+        MonetDB-SQL
+        MonetDB-server
+        MonetDB-client
+        MonetDB-embedded
+        MonetDB-devel
+        MonetDB-embedded-devel
+    )
+    yum -y install "${pkgs[@]/%/-$REQUESTED_MONETDB_VERSION}"
+}
+
+# Install the packages
+if type -P yum >/dev/null; then
+    install_rpms
+elif type -P apt-get >/dev/null; then
+    install_debs
+else
+    echo 'Cannot find yum or apt-get'
+    exit 1
+fi
+
+
+# Print metadata
 echo "prefix=/usr" >>github.output
 echo "bindir=/usr/bin"  >>github.output
-echo "includedir=$(pkg-config --variable=includedir zlib)/monetdb" >>github.output
-echo "libdir=$(pkg-config --variable=libdir zlib)" >>github.output
+echo "includedir=/usr/include/monetdb" >>github.output
+echo "libdir=$(pkg-config --variable=libdir monetdbe)" >>github.output
 echo "dynsuffix=so" >>github.output
+
 
 # Start and create database
 sudo systemctl enable monetdbd
